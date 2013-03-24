e6abf319565ad08c6608874b3330c134e42adef7
Yaron Hagai <yaron.hagai@fiverr.com>
2013-03-24 08:31:50 +0200

    Finish auth exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,13 +41,7 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-
-      end
+      before {valid_signup}
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -58,7 +52,7 @@ describe "User pages" do
         let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
         it { should have_link('Sign out') }
         describe "followed by signout" do
           before { click_link "Sign out" }
---
+++ spec/support/user_pages_utils.rb
@@ -0,0 +1,16 @@
+module UserPagesUtils
+
+   def valid_signup
+    fill_in "Name",         with: "Example User"
+    fill_in "Email",        with: "user@example.com"
+    fill_in "Password",     with: "foobar"
+    fill_in "Confirmation", with: "foobar"
+  end
+
+
+  RSpec::Matchers.define :have_success_message do |message|
+    match do |page|
+      page.should have_selector('div.alert.alert-success', text: message)
+    end
+  end
+end
