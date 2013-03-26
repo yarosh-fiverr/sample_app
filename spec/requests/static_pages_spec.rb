45007bfb07a67bd2ecae47e41d59e583d334626f
Yaron Hagai <yaron.hagai@fiverr.com>
2013-03-26 10:02:42 +0200

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -47,6 +47,18 @@ describe "Static pages" do
         end
       end
 
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+
+
       describe "pagination" do
 
       before(:all) { 30.times { FactoryGirl.create(:micropost, user: user, content: "just for pagination") } }
